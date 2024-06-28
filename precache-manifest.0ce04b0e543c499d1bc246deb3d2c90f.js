1228717e16c50166cb4ab27038af3689886c7c6a
Rizz0S <Rizz0S@users.noreply.github.com>
2024-06-28 03:54:13 +0000

    deploy: ce37bb6ac3c162cf11af5eda88f33793286c1b1b

---
+++ precache-manifest.0ce04b0e543c499d1bc246deb3d2c90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5ac0077af30bfe40935c53b16829d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c646d110ada9cbbd718",
+    "url": "/static/css/main.9074224c.chunk.css"
+  },
+  {
+    "revision": "fec37ab72fb467fc698b",
+    "url": "/static/js/2.28440d80.chunk.js"
+  },
+  {
+    "revision": "4d1e2c37fe0926bdaf9c7f519162bc84",
+    "url": "/static/js/2.28440d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c646d110ada9cbbd718",
+    "url": "/static/js/main.6393b972.chunk.js"
+  },
+  {
+    "revision": "594355fb86b719fd11bd",
+    "url": "/static/js/runtime-main.53e8a7b8.js"
+  }
+]);
